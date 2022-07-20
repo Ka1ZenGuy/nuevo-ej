d4a5d0f959b8267d889c7b01ffc186c0399d4a89
Ka1ZenGuy <edgargexpositohdez@gmail.com>
2022-07-20 13:27:43 +0100

    .

---
+++ ejemplo-buscador/ejemplo-buscador.js
@@ -0,0 +1,353 @@
+let datos = [{
+    "nombre_especie": "Lamprotornis nitens",
+    "recuento": 39
+}, {
+    "nombre_especie": "Toxostoma curvirostre",
+    "recuento": 88
+}, {
+    "nombre_especie": "Zosterops pallidus",
+    "recuento": 51
+}, {
+    "nombre_especie": "Alectura lathami",
+    "recuento": 13
+}, {
+    "nombre_especie": "Bubalornis niger",
+    "recuento": 6
+}, {
+    "nombre_especie": "Lepilemur rufescens",
+    "recuento": 15
+}, {
+    "nombre_especie": "Dasyurus viverrinus",
+    "recuento": 18
+}, {
+    "nombre_especie": "Geospiza sp.",
+    "recuento": 62
+}, {
+    "nombre_especie": "Loris tardigratus",
+    "recuento": 46
+}, {
+    "nombre_especie": "Haliaeetus leucocephalus",
+    "recuento": 36
+}, {
+    "nombre_especie": "Lepilemur rufescens",
+    "recuento": 47
+}, {
+    "nombre_especie": "Laniaurius atrococcineus",
+    "recuento": 70
+}, {
+    "nombre_especie": "Cabassous sp.",
+    "recuento": 65
+}, {
+    "nombre_especie": "Phalacrocorax brasilianus",
+    "recuento": 45
+}, {
+    "nombre_especie": "Pelecanus occidentalis",
+    "recuento": 55
+}, {
+    "nombre_especie": "Gymnorhina tibicen",
+    "recuento": 12
+}, {
+    "nombre_especie": "Geochelone elephantopus",
+    "recuento": 69
+}, {
+    "nombre_especie": "Phalaropus fulicarius",
+    "recuento": 61
+}, {
+    "nombre_especie": "Tiliqua scincoides",
+    "recuento": 42
+}, {
+    "nombre_especie": "Microcebus murinus",
+    "recuento": 72
+}, {
+    "nombre_especie": "Sciurus niger",
+    "recuento": 25
+}, {
+    "nombre_especie": "Sarkidornis melanotos",
+    "recuento": 33
+}, {
+    "nombre_especie": "Canis aureus",
+    "recuento": 93
+}, {
+    "nombre_especie": "Echimys chrysurus",
+    "recuento": 1
+}, {
+    "nombre_especie": "Choloepus hoffmani",
+    "recuento": 8
+}, {
+    "nombre_especie": "Tachybaptus ruficollis",
+    "recuento": 3
+}, {
+    "nombre_especie": "Platalea leucordia",
+    "recuento": 85
+}, {
+    "nombre_especie": "Tragelaphus strepsiceros",
+    "recuento": 75
+}, {
+    "nombre_especie": "Theropithecus gelada",
+    "recuento": 21
+}, {
+    "nombre_especie": "Alligator mississippiensis",
+    "recuento": 73
+}, {
+    "nombre_especie": "Vanellus sp.",
+    "recuento": 90
+}, {
+    "nombre_especie": "Chlidonias leucopterus",
+    "recuento": 80
+}, {
+    "nombre_especie": "Lycosa godeffroyi",
+    "recuento": 24
+}, {
+    "nombre_especie": "Gabianus pacificus",
+    "recuento": 59
+}, {
+    "nombre_especie": "Oryx gazella",
+    "recuento": 11
+}, {
+    "nombre_especie": "Oryx gazella",
+    "recuento": 74
+}, {
+    "nombre_especie": "Gabianus pacificus",
+    "recuento": 6
+}, {
+    "nombre_especie": "Streptopelia senegalensis",
+    "recuento": 84
+}, {
+    "nombre_especie": "Pavo cristatus",
+    "recuento": 84
+}, {
+    "nombre_especie": "Isoodon obesulus",
+    "recuento": 47
+}, {
+    "nombre_especie": "Boa constrictor mexicana",
+    "recuento": 53
+}, {
+    "nombre_especie": "Sylvicapra grimma",
+    "recuento": 64
+}, {
+    "nombre_especie": "Trachyphonus vaillantii",
+    "recuento": 62
+}, {
+    "nombre_especie": "Colaptes campestroides",
+    "recuento": 39
+}, {
+    "nombre_especie": "Tamandua tetradactyla",
+    "recuento": 67
+}, {
+    "nombre_especie": "Cervus canadensis",
+    "recuento": 5
+}, {
+    "nombre_especie": "Felis wiedi or Leopardus weidi",
+    "recuento": 53
+}, {
+    "nombre_especie": "Geococcyx californianus",
+    "recuento": 17
+}, {
+    "nombre_especie": "Mungos mungo",
+    "recuento": 93
+}, {
+    "nombre_especie": "Oryx gazella",
+    "recuento": 4
+}, {
+    "nombre_especie": "Antidorcas marsupialis",
+    "recuento": 79
+}, {
+    "nombre_especie": "Lasiodora parahybana",
+    "recuento": 95
+}, {
+    "nombre_especie": "Rhea americana",
+    "recuento": 24
+}, {
+    "nombre_especie": "Phalaropus fulicarius",
+    "recuento": 71
+}, {
+    "nombre_especie": "Hippopotamus amphibius",
+    "recuento": 2
+}, {
+    "nombre_especie": "Physignathus cocincinus",
+    "recuento": 38
+}, {
+    "nombre_especie": "Pelecans onocratalus",
+    "recuento": 5
+}, {
+    "nombre_especie": "Conolophus subcristatus",
+    "recuento": 85
+}, {
+    "nombre_especie": "Nannopterum harrisi",
+    "recuento": 14
+}, {
+    "nombre_especie": "Certotrichas paena",
+    "recuento": 1
+}, {
+    "nombre_especie": "Lasiodora parahybana",
+    "recuento": 40
+}, {
+    "nombre_especie": "Anitibyx armatus",
+    "recuento": 58
+}, {
+    "nombre_especie": "Bubalus arnee",
+    "recuento": 76
+}, {
+    "nombre_especie": "Macaca mulatta",
+    "recuento": 36
+}, {
+    "nombre_especie": "Petaurus norfolcensis",
+    "recuento": 15
+}, {
+    "nombre_especie": "Ceratotherium simum",
+    "recuento": 70
+}, {
+    "nombre_especie": "Ammospermophilus nelsoni",
+    "recuento": 38
+}, {
+    "nombre_especie": "Seiurus aurocapillus",
+    "recuento": 21
+}, {
+    "nombre_especie": "Bison bison",
+    "recuento": 15
+}, {
+    "nombre_especie": "Chelodina longicollis",
+    "recuento": 9
+}, {
+    "nombre_especie": "Naja haje",
+    "recuento": 96
+}, {
+    "nombre_especie": "Spizaetus coronatus",
+    "recuento": 92
+}, {
+    "nombre_especie": "Dicrurus adsimilis",
+    "recuento": 30
+}, {
+    "nombre_especie": "Coendou prehensilis",
+    "recuento": 2
+}, {
+    "nombre_especie": "Macropus rufogriseus",
+    "recuento": 96
+}, {
+    "nombre_especie": "Isoodon obesulus",
+    "recuento": 77
+}, {
+    "nombre_especie": "Eubalaena australis",
+    "recuento": 56
+}, {
+    "nombre_especie": "Choloepus hoffmani",
+    "recuento": 6
+}, {
+    "nombre_especie": "Phalacrocorax niger",
+    "recuento": 100
+}, {
+    "nombre_especie": "Mycteria ibis",
+    "recuento": 21
+}, {
+    "nombre_especie": "Perameles nasuta",
+    "recuento": 53
+}, {
+    "nombre_especie": "Hystrix cristata",
+    "recuento": 47
+}, {
+    "nombre_especie": "Conolophus subcristatus",
+    "recuento": 92
+}, {
+    "nombre_especie": "Plegadis ridgwayi",
+    "recuento": 53
+}, {
+    "nombre_especie": "Phasianus colchicus",
+    "recuento": 19
+}, {
+    "nombre_especie": "Balearica pavonina",
+    "recuento": 59
+}, {
+    "nombre_especie": "Canis lupus",
+    "recuento": 99
+}, {
+    "nombre_especie": "Paroaria gularis",
+    "recuento": 57
+}, {
+    "nombre_especie": "Sciurus vulgaris",
+    "recuento": 48
+}, {
+    "nombre_especie": "Echimys chrysurus",
+    "recuento": 57
+}, {
+    "nombre_especie": "Ardea cinerea",
+    "recuento": 93
+}, {
+    "nombre_especie": "Gerbillus sp.",
+    "recuento": 32
+}, {
+    "nombre_especie": "Panthera leo persica",
+    "recuento": 13
+}, {
+    "nombre_especie": "unavailable",
+    "recuento": 94
+}, {
+    "nombre_especie": "Macaca mulatta",
+    "recuento": 28
+}, {
+    "nombre_especie": "Turtur chalcospilos",
+    "recuento": 59
+}, {
+    "nombre_especie": "Ploceus intermedius",
+    "recuento": 90
+}, {
+    "nombre_especie": "Connochaetus taurinus",
+    "recuento": 80
+}, {
+    "nombre_especie": "Echimys chrysurus",
+    "recuento": 46
+}, {
+    "nombre_especie": "Macropus robustus",
+    "recuento": 39
+}]
+let orden = "ascendente"
+function ascendente() {
+    orden = "ascendente";
+    actualizarLista();
+
+}
+
+function descendente() {
+    orden = "descendente";
+    actualizarLista();
+}
+
+function actualizarLista() {
+
+    let listaordenada = []
+
+
+    if (orden === "ascendente") {
+        listaordenada = datos.sort(
+            (a, b) => {
+            if (a.nombre_especie < b.nombre_especie) return -1;
+            if (a.nombre_especie > b.nombre_especie) return 1;
+            return 0;
+        })
+
+    } else if (orden === "descendente") {
+        listaordenada = [...datos].sort(
+            (a, b) => {
+                if (a.nombre_especie > b.nombre_especie) return -1;
+                if (a.nombre_especie < b.nombre_especie) return 1;
+                return 0;
+            }
+
+        )
+    }
+    else {
+        listaordenada = [...datos];
+    }
+    {
+        document.getElementById("lista").innerHTML =
+            listaordenada
+                .map(
+                    x =>
+                        `<div> 
+    ${x.nombre_especie} (${x.recuento})
+    </div>`
+
+                ).join("\n")
+    }
+}
+
+actualizarLista(datos);
